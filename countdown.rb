90783cb1191bdffebc76d747642935b08e4f9c27
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-07-24 16:07:21 -0400

    commit tree 6d9f46e7a55389b143c914a9f84b44331515b509

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    number = 11
+    until number == 1
+        number -= 1
+        countdown_with_sleep
+        puts "#{number} SECOND(S)!"
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_secs = 1)
+    sleep(num_secs)
 end
